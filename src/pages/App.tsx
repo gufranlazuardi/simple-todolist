7b2e99ce996c197354279b6cda042d66a07982cb
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-11 02:43:17 +0700

    fix: main page

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import TodoCard from "@/components/TodoCard";
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex justify-center pt-[10rem] pb-[18rem] h-full w-screen">
+        <TodoCard />
+      </div>
+    </>
+  );
+};
+
+export default Home;
